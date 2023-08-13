3fac9b218f42ab3eac5e245db9f7be65e256adae
prateekchhibber <0392pc@gmail.com>
2023-08-13 17:14:24 +0530

    Todo management system project

---
+++ src/main/java/com/ernest/todomanagement/TodoManagementApplication.java
@@ -0,0 +1,14 @@
+package com.ernest.todomanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
